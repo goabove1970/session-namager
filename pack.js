0bbc9d9661b08ddd2885eb83f56283f25d1a7c47
Ievgen Melnychuk <ievgenmelnychuk@Ievgens-MacBook.local>
2020-07-24 15:46:19 -0400

    Updated packing scripts

--- pack.js
+++ pack.js
@@ -4,15 +4,17 @@ const zipFolder = require('zip-folder');
 exec('node -p -e "require(\'./package.json\').version"', (error, version, stderr) => {
   exec('node -p -e "require(\'./package.json\').name"', (error, name, stderr) => {
     const packageName = name.substr(0, name.length - 1);
-    const versionStr = version.substr(0, version.length - 1);
-    console.log(`Packing ${packageName} version ${versionStr}`);
-    const packageFileName = `${packageName}-${versionStr}.zip`;
-    zipFolder('.', packageFileName, function(err) {
-      if (err) {
-        console.log('Packing failed', err);
-      } else {
-        console.log('Packing complete');
-      }
+    exec(`rm -rf ${packageName}-*.zip`, (error, stdout, stderr) => {
+      const versionStr = version.substr(0, version.length - 1);
+      console.log(`Packing ${packageName} version ${versionStr}`);
+      const packageFileName = `${packageName}-${versionStr}.zip`;
+      zipFolder('.', packageFileName, function(err) {
+        if (err) {
+          console.log('Packing failed', err);
+        } else {
+          console.log('Packing complete');
+        }
+      });
     });
   });
 });
